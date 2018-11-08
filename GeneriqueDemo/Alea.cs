80fcce8a6ea757f4629c6c57d0e38c68bf7d40c5
joyet-simon <joyet.simon@hotmail.fr>
2018-11-08 09:01:06 +0100

    Ajoutez des fichiers projet.

---
+++ GeneriqueDemo/Alea.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneriqueDemo
+{
+    class Alea<T>
+    {
+        private List<T> items = new List<T>();
+
+        public void add(T obj)
+        {
+            items.Add(obj);
+        }
+
+        public T getAleaUnique()
+        {
+            int no = (new Random()).Next(0, items.Count);
+            T temp = items[no];
+            items.Remove(items[no]);
+            return temp;
+        }
+
+        public T getAlea()
+        {
+            int no = (new Random()).Next(0, items.Count);
+            return items[no];
+        }
+    }
+}
---
+++ GeneriqueDemo/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeneriqueDemo
+{
+    public partial class Form1 : Form
+    {
+        Int32 nbrClick = 5;
+        Alea<Int32> loto = new Alea<int>();
+        Alea<Int32> loto2 = new Alea<int>();
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i <= 50; i++) { loto.add(i); }
+            for (int i = 1; i <= 10; i++) { loto2.add(i); }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Int32 num = loto.getAleaUnique();
+            nbrClick--;
+            textBox1.AppendText(num.ToString() + ". ");
+            if (nbrClick == 0)
+            {
+                Int32 numComp = loto2.getAleaUnique();
+                textBox1.AppendText("-> " + numComp.ToString() + ". ");
+                button1.Enabled = false;
+            }
+        }
+
+    }
+}
